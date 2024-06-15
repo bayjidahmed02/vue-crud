b25f92231a1f9621f731cd4ea6c421dd9a9fef7e
Bayjid Ahmed <bayjidahmed01@gmail.com>
2024-06-15 13:36:52 +0600

    created without validation

--- api.php
+++ api.php
@@ -1,8 +1,8 @@
 <?php
-// header('content-type: application/json');
+header('content-type: application/json');
 header("Access-Control-Allow-Origin: *");
-// header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
 
@@ -22,11 +22,40 @@ if ($action === 'read') {
         array_push($users, $row);
     }
     $response['users'] = $users;
+} else if ($action === 'create') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+
+    $result = $conn->query("INSERT INTO `users` (`name`,`email`,`phone`) VALUES ('$name','$email','$phone')");
+    if ($result) {
+        $response['msg'] = 'Data Added Successfully';
+    } else {
+        $response['msg'] = 'Something error';
+    }
+} else if ($action === 'delete') {
+    $id = $_GET['id'];
+
+    $result = $conn->query("DELETE FROM users WHERE id = $id");
+    if ($result) {
+        $response['msg'] = 'Data Deleted Successfully';
+    } else {
+        $response['msg'] = 'Something error';
+    }
+} else if ($action == 'update') {
+
+    $id = $_GET['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+
+    $result = $conn->query("UPDATE `users` SET `name`='$name',`email`='$email',`phone`='$phone' WHERE `id`=$id");
+    if ($result) {
+        $response['msg'] = 'Data Updated Successfully';
+    } else {
+        $response['msg'] = 'Something error';
+    }
 }
-
-
-
-
 
 
 echo json_encode($response);
--- main.js
+++ main.js
@@ -2,13 +2,42 @@ const app = Vue.createApp({
   data() {
     return {
       users: [],
+      form: {
+        name: "",
+        email: "",
+        phone: "",
+      },
+      addModal: false,
     };
   },
   methods: {
+    showModal() {
+      this.addModal = true;
+    },
+    hideModal() {
+      this.addModal = false;
+    },
     getData() {
       axios.get("http://localhost/vue/api.php?action=read").then((res) => {
         this.users = res.data.users;
       });
+    },
+    addData() {
+      let formData = new FormData();
+      formData.append("name", this.form.name);
+      formData.append("email", this.form.email);
+      formData.append("phone", this.form.phone);
+      axios
+        .post("http://localhost/vue/api.php?action=create", formData)
+        .then((res) => {
+          // this.users = res.data.users;
+          console.log(res.data);
+          this.getData();
+          this.hideModal();
+          this.form.name = "";
+          this.form.email = "";
+          this.form.phone = "";
+        });
     },
   },
   mounted() {
